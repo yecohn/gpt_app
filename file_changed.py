10e9eca0ed93bcc887e427e335964f2e18159eaf
Yehoshua Cohen <yosh@Yehoshuas-MacBook-Air.local>
2023-04-14 18:03:28 +0300

    remove realistic voice

--- file_changed.py
+++ file_changed.py
@@ -4,21 +4,22 @@ from TTS.utils.manage import ModelManager
 from TTS.utils.synthesizer import Synthesizer
 import site
 location = site.getsitepackages()[0]
+from gtts import gTTS
 
 path = location+"/TTS/.models.json"
 
-model_manager = ModelManager(path)
+# model_manager = ModelManager(path)
 
-model_path, config_path, model_item = model_manager.download_model("tts_models/en/ljspeech/tacotron2-DDC")
+# model_path, config_path, model_item = model_manager.download_model("tts_models/en/ljspeech/tacotron2-DDC")
 
-voc_path, voc_config_path, _ = model_manager.download_model(model_item["default_vocoder"])
+# voc_path, voc_config_path, _ = model_manager.download_model(model_item["default_vocoder"])
 
-synthesizer = Synthesizer(
-    tts_checkpoint=model_path,
-    tts_config_path=config_path,
-    vocoder_checkpoint=voc_path,
-    vocoder_config=voc_config_path
-)
+# synthesizer = Synthesizer(
+#     tts_checkpoint=model_path,
+#     tts_config_path=config_path,
+#     vocoder_checkpoint=voc_path,
+#     vocoder_config=voc_config_path
+# )
 
 
 with open("output.txt", "r") as f1: 
@@ -31,8 +32,11 @@ while 1:
         if new_input != input: 
             start = time.time()
             print("file has changed")
-            outputs = synthesizer.tts(new_input)
-            synthesizer.save_wav(outputs, "speech.wav")
+            audio = gTTS(text=new_input, lang="en", slow=False)
+            audio.save("speech.wav")
+            os.system("afplay speech.wav")
+            # outputs = synthesizer.tts(new_input)
+            # synthesizer.save_wav(outputs, "speech.wav")
             end = time.time()
             input = new_input
             print(f"inference time: {end - start}")
